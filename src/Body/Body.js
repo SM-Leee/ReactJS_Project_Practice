b2ab385a8dcdef07b614bbcb780beb0d0f6407b3
bitacademy-douzone <tjdals1708@naver.com>
2019-04-23 11:12:00 +0900

    미완성 코드

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import TextBoxBasic from './InputBox';
 import './Body.css';
+import DsDatePicker from './DsDatePicker';
 
 class Body extends Component {
     render() {
         return (
             <div className="Body">
-            {/* default값음 width="long" height="short" 이다 */}
-            {/* kwd 가 true 이면 won image가 뜨게 하시오 */}
-                <TextBoxBasic title ="일   자" width="short"/>
-                <TextBoxBasic title="단    가" width="short" kwd='true'/>
-                <TextBoxBasic title ="공급가" width="short"/>
-                <TextBoxBasic title="VAT" width="short"/>
-                <TextBoxBasic title="TEST" kwd='true'/>
-                <TextBoxBasic title="TEST2"/>
-                <TextBoxBasic title="상세사항" height='long'/>
-                <TextBoxBasic title="금   액" width='short' kwd='true'/>
+                {/* default값음 width="long" height="short" 이다 */}
+                {/* kwd 가 true 이면 won image가 뜨게 하시오 */}
+                
+                <DsDatePicker title="일  자" width="short" />
+                <TextBoxBasic title="분  류" width="short" />
+                <TextBoxBasic title="거래처" />
+                <TextBoxBasic title="상  품" />
+                <TextBoxBasic title="수  량" width="short" />
+                <TextBoxBasic title="단  가" width="short" kwd="true" />
+                <TextBoxBasic title="공급가" width="short" kwd="true" />
+                <TextBoxBasic title="VAT" width="short" kwd="true" />
+                <TextBoxBasic title="상세설명" height="long" />
+
             </div>
         );
     }
---
+++ src/Body/DsDatePicker/DsDatePicker.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import './DsDatePicker.css';
+import styles from './DsDatePicker.css';
+import classNames from 'classnames/bind';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const cn = classNames.bind(styles);
+
+class DsDatePicker extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDate: new Date()
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+    handleChange(date) {
+        this.setState({
+            startDate: date
+        });
+    }
+    render() {
+
+        const { width, title } = this.props;
+        const short = "short";
+        // eslint-disable-next-line eqeqeq
+        const widthCol = ((width == short) ? 2 : 1);
+        // eslint-disable-next-line eqeqeq
+        const widthProve = ((width == short) ? false : true);
+        const widthSize = (100 / widthCol) + '%';
+        const style = {
+            width: widthSize
+        }
+
+        return (
+            <div className="dsDatePicker" style={style}>
+                <div className={cn('dsDatePickerTitleMiddle', { widthProve })}>
+                    <div className="dsDatePickerTitle">
+                        <span>{title}</span>
+                    </div>
+                </div>
+                <div className={cn('dsDatePickerInputFull', { widthProve })}>
+                    <DatePicker
+                         selected={this.state.startDate}
+                        onChange={this.handleChange}
+                        dateFormat="yyyy-MM-dd"
+                    />
+                    {/* <div className={cn('dsDatePickerImages')}></div> */}
+                </div>
+            </div>
+
+
+        );
+    }
+}
+
+export default DsDatePicker;
